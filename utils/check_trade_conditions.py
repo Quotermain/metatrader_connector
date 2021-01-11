2380c639f435e2d827e6fc6790cebf7ba8fc2d26
Quotermain <pavelhat@gmail.com>
2021-01-11 18:20:50 +0500

    First commit

--- utils/check_trade_conditions.py
+++ utils/check_trade_conditions.py
@@ -9,34 +9,26 @@ def check_trade_conditions(ticker, df_day, df_5min):
 
     condition_short = (
         (df_day.close[-1] - df_day.open[-1]) > 0 and
-        (df_5min.close[-2] - df_5min.open[-2]) > 0 and
+        (df_5min.close[-1] - df_5min.open[-1]) > 0 and
         (
             (df_day.close[-1] - df_day.open[-1]) / df_day.open[-1] >
             dict_open_close_day_dif[ticker]
         ) and
         (
-            (df_5min.close[-2] - df_5min.open[-2]) / df_5min.open[-2] >
-            dict_open_close_5min_dif[ticker]
-        ) and
-        (
-            abs((df_5min.close[-1] - df_5min.open[-1])) /   df_5min.open[-1] <
+            (df_5min.close[-1] - df_5min.open[-1]) / df_5min.open[-1] >
             dict_open_close_5min_dif[ticker]
         )
     )
 
     condition_long = (
         (df_day.close[-1] - df_day.open[-1]) < 0 and
-        (df_5min.close[-2] - df_5min.open[-2]) < 0 and
+        (df_5min.close[-1] - df_5min.open[-1]) < 0 and
         (
             (df_day.open[-1] - df_day.close[-1]) / df_day.close[-1] >
             dict_open_close_day_dif[ticker]
         ) and
         (
-            (df_5min.open[-2] - df_5min.close[-2]) / df_5min.close[-2] >
-            dict_open_close_5min_dif[ticker]
-        ) and
-        (
-            abs((df_5min.open[-1] - df_5min.close[-1])) / df_5min.close[-1] <
+            (df_5min.open[-1] - df_5min.close[-1]) / df_5min.close[-1] >
             dict_open_close_5min_dif[ticker]
         )
     )
